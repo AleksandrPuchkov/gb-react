79e4abcde9c9366b47623b96c57886fcb2f6f26c
Aleksandr <iwolframium@bk.ru>
2022-04-10 10:24:51 +0300

    fixes after review

--- src/components/FormHW1/Form.jsx
+++ src/components/FormHW1/Form.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
+import './styles/Form.css'
 import { Message } from './Message'
 import { Input } from './Input'
 import { Button } from './Button'
 
 export const Form = () => {
-
     const [value, setValue] = useState('')
     const [message, setMessage] = useState('Это поле Message, и в нем отобразится новое сообщение')
 
