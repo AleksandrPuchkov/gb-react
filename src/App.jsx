3cf49878a98f054fb7c8a9a58051afcf13ded0b3
aleksandrpuchkov <iwolframium@bk.ru>
2022-04-17 18:40:38 +0300

    lesson-2 task

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
-import './App.css';
-import { Form } from './components/Form.jsx'
+import React from 'react'
+import { Form } from './components/Form/Form'
+import './App.css'
 
- export class App extends Component {
-   render () {
-     return <Form />
-   }
- }
+export const App = () => {
+
+  return <Form />
+
+}
---
+++ src/components/MessageList/MessageList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './MessageList.css'
+
+export const MessageList = ({ messages }) => (
+    <ul>
+        {messages.map((message, idx) => {
+            if (message.username === "Chatbot")
+                return (
+                    <li key={idx} className="message botmessage"><h3 className="botname">{message.username}</h3> <p className="bottext">{message.message}</p></li>)
+            else return (
+                <li key={idx} className="message usermessage"><h3 className="username">{message.username}</h3> <p className="usertext">{message.message}</p></li>)
+        })}
+    </ul>
+)
