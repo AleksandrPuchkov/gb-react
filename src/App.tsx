7ef3a7254529a92ab5c6c299592f6b49629edfb4
aleksandrpuchkov <iwolframium@bk.ru>
2022-04-22 02:28:39 +0300

    header + routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React, { FC } from 'react';
-import { Form } from './components/Form/Form';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Profile } from './pages/Profile'
+import { Header } from './components/Header/Header';
 import './App.css';
+import { Chats } from './pages/Chats';
 
 export const App: FC = () => {
-  return <Form />;
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="profile" element={<Profile />} />
+
+
+        <Route path="chats">
+          <Route index element={<Chats />} />
+        </Route>
+
+      </Route>
+      <Route path="*" element={<h2>404</h2>} />
+    </Routes>
+  </BrowserRouter >
 };
